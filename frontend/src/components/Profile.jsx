1defa099ec798164700cb09bee2eaee04127c205
Dan Frunza <dani.frunza@yahoo.com>
2026-01-07 16:16:46 +0200

    Profile

---
+++ frontend/src/components/Profile.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from "react";
+import "../css/Profile.css";
+
+
+const Profile = () => {
+    return (
+        <main className="profile">
+            <h1>Profile Page</h1>
+        </main>
+    );
+};
+
+export default Profile;
