0f4f2a82e18725f3bb8f86a7eb4d729ad02b713b
Dan Frunza <dani.frunza@yahoo.com>
2026-01-10 19:50:55 +0200

    Small ajustment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { BrowserRouter as Router } from 'react-router-dom'
+import { HashRouter as Router } from 'react-router-dom'
 import Navbar from './components/Navbar.jsx'
 import AppRoutes from './components/AppRoutes.jsx'
  
